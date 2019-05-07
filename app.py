18918c7f5a563dead8a1ea7994455a40cf5578c6
baroncurtin2 <knowledge3@gmail.com>
2019-05-06 23:22:46 -0400

    added dropdown

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from data import NBAData
 # get data source
 nba = NBAData()
 data = nba.data
+players = [{'label': p, 'value': p} for p in nba.player_names]
 
 # dash setup
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -19,7 +20,19 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # dash layout
+app.layout = html.Div(children=[
+    html.H1('NBA Best Mid-Range Shooters'),
 
+    html.Div(children=[
+        html.H5('Player Name'),
+
+        dcc.Dropdown(
+            id='player-selector',
+            options=players,
+            value=players[0]['value']
+        )
+    ])
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)

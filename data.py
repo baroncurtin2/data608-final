dcfedc7c3ef2c02b2ed2ae3150dd208e1ebdc4bc
baroncurtin2 <knowledge3@gmail.com>
2019-05-10 19:54:49 -0400

    custom headers

--- data.py
+++ data.py
@@ -8,8 +8,20 @@ class NBAData:
         self.__player_names = self.__data['PLAYER_NAME'].unique()
 
     def __get_data(self):
+        # custom headers
+        custom_headers = {
+            'Host': 'stats.nba.com',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
+
         # Basic Request
-        shot_locations = LeagueDashPlayerShotLocations(distance_range='5ft Range')
+        shot_locations = LeagueDashPlayerShotLocations(distance_range='5ft Range', headers=custom_headers)
         nba_data = shot_locations.data_sets[0].data['data']
 
         # fix headers
